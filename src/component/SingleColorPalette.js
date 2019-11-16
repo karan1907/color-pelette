a741ab5c92fd2e489a741305c251ac27fd47279e
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-11-16 06:59:10 +0530

    Added Go Back Link On SinglePalette Colour

--- src/component/SingleColorPalette.js
+++ src/component/SingleColorPalette.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ColourBox from "./colourBox";
+import { Link } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import PaletteFooter from "./layout/PaletteFooter";
 
@@ -26,19 +27,26 @@ export default class SingleColorPalette extends Component {
   }
   render() {
     const { format } = this.state;
-    const { paletteName, emoji } = this.props.palette;
+    const { paletteName, emoji, id } = this.props.palette;
     const colorBoxes = this._shades.map(color => (
       <ColourBox
-        key={color.id}
+        key={color.name}
         name={color.name}
         background={color[format]}
         showLink={false}
       />
     ));
     return (
-      <div className="Palette">
+      <div className="SingleColorPalette Palette">
         <Navbar handleChange={this.changeFormat} showingAllColors={false} />
-        <div className="Palette-colors">{colorBoxes}</div>
+        <div className="Palette-colors">
+          {colorBoxes}
+          <div className="go-back ColorBox">
+            <Link to={`/palette/${id}`} className="back-button">
+              Go Back
+            </Link>
+          </div>
+        </div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
